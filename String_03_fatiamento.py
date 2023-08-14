79760dfacf5f7419e6be772f4b4b91446fa58def
valdenizio-brasil <valdenizio.brasil@gmail.com>
2023-08-14 19:17:02 -0300

    Strings

---
+++ String_03_fatiamento.py
@@ -0,0 +1,20 @@
+nome = "Guilherme Arthur de Carvalho"
+meu_nome = "Valdenizio de Albuquerque Brasil"
+
+print(nome[0])
+print(nome[-2])
+print(nome[:9])
+print(nome[10:])
+print(nome[10:16])
+print(nome[10:16:2])
+print(nome[:])
+print(nome[::-1])
+
+print(meu_nome[0])
+print(meu_nome[-2])
+print(meu_nome[:9])
+print(meu_nome[10:])
+print(meu_nome[10:16])
+print(meu_nome[10:16:2])
+print(meu_nome[:])
+print(meu_nome[::-1])
